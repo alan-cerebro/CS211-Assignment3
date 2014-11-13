8e8600917088a27322e004e5615365fae201ae9d
Alan Albino <alan.albino6@gmail.com>
2014-11-13 11:46:04 -0800

    Initial Commit

---
+++ Assignment3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment3
+//
+//  Created by Alan Albino on 11/13/14.
+//  Copyright (c) 2014 AlanAlbino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
